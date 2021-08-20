60a0fc79c2c92bffbf9c4b32be84c7e21dcfb0d7
rezwan <rezwan6@gmail.com>
2021-08-21 02:25:41 +0600

    solved for basic requirements

--- js/app.js
+++ js/app.js
@@ -16,39 +16,54 @@ const grandTotal = document.getElementById('grand-total');
 
 // calculating total price
 function totalCost() {
-
+    const bestPriceAmount = parseFloat(bestPrice.innerText);
+    const memoryCost = document.getElementById('memory-cost');
+    const memoryCostAmount = parseFloat(memoryCost.innerText);
+    const storageCost = document.getElementById('storage-cost');
+    const storageCostAmount = parseFloat(storageCost.innerText);
+    const deliveryCost = document.getElementById('delivery-cost');
+    const deliveryCostAmount = parseFloat(deliveryCost.innerText);
+    const totalCostAmount = bestPriceAmount + memoryCostAmount + storageCostAmount + deliveryCostAmount;
+    totalPrice.innerText = totalCostAmount;
 }
 
 // add event listener to the memory buttons
 memoryLow.addEventListener('click', function () {
     const memoryCost = document.getElementById('memory-cost');
     memoryCost.innerText = '0';
+    totalCost();
 })
 memoryHigh.addEventListener('click', function () {
     const memoryCost = document.getElementById('memory-cost');
     memoryCost.innerText = '180';
+    totalCost();
 })
 
 // add event listener to the storage buttons
 storageLow.addEventListener('click', function () {
     const storageCost = document.getElementById('storage-cost');
     storageCost.innerText = '0';
+    totalCost();
 })
 storageMid.addEventListener('click', function () {
     const storageCost = document.getElementById('storage-cost');
     storageCost.innerText = '100';
+    totalCost();
 })
 storageHigh.addEventListener('click', function () {
     const storageCost = document.getElementById('storage-cost');
     storageCost.innerText = '180';
+    totalCost();
 })
 
 // add event listener to the delivery buttons
 deliveryFree.addEventListener('click', function () {
     const deliveryCost = document.getElementById('delivery-cost');
     deliveryCost.innerText = '0';
+    totalCost();
 })
 deliveryExpress.addEventListener('click', function () {
     const deliveryCost = document.getElementById('delivery-cost');
     deliveryCost.innerText = '20';
+    totalCost();
 })
